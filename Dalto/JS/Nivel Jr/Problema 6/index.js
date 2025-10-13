3181a8461374e43230e2545714799fd1704b540e
gonzalo aime <aimegonzalo@gmail.com>
2025-10-13 17:55:42 +0000

    ejercicio llaves en proceso

---
+++ Dalto/JS/Nivel Jr/Problema 6/index.js
@@ -0,0 +1,29 @@
+//  Indicarle a nuestro usuario las 20 llaves posibles con sus imágenes para que la pueda seleccionar
+//  Una vez seleccionada, enviar los datos al servidor y que otro programador se encargue
+const contenedor = document.querySelector(".flex-container");
+
+const crearLlave = (nombre, modelo, precio) => {
+  img = "<img src='llave.png' class= 'llave'>";
+  nombre = `<h2>${nombre}</h2>`;
+  modelo = `<h3>${modelo}</h3>`;
+  precio = `<p>Precio:<b>$${precio}</b></p>`;
+  return [img, nombre, modelo, precio];
+};
+
+let documentFragment = document.createDocumentFragment();
+for (var i = 0; i <= 20; i++) {
+  let modeloRandom = Math.round(Math.random() * 10000);
+  let precioRandom = Math.round(Math.random() * 10 + 30);
+  let llave = crearLlave(
+    `llave ${i}`,
+    `modelo ${modeloRandom}`,
+    `${precioRandom}`
+  );
+  let div = document.createElement("DIV");
+  div.classList.add(`item-${i}`, "flex-item");
+  div.innerHTML = llave[0] + llave[1] + llave[2] + llave[3];
+  documentFragment.appendChild(div);
+  // contenedor.innerHTML += div;
+}
+contenedor.appendChild(documentFragment);
+// contenedor.innerHTML = llave[0] + llave[1] + llave[2];
