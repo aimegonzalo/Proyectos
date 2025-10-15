12a398288173462dc7b9a4a49f676d797c2e8183
gonzalo aime <aimegonzalo@gmail.com>
2025-10-15 22:38:55 +0000

    problema 7 parte 2 completada

--- Dalto/JS/Nivel Intermedio/Problema 1/index.js
+++ Dalto/JS/Nivel Intermedio/Problema 1/index.js
@@ -4,18 +4,32 @@
 //          - Debe ser al menos full hd
 //          - Cuando la esté por comprar, preguntarle si está seguro
 
-let alto = window.screen.availHeight;
-let ancho = window.screen.availWidth;
+// let alto = window.screen.availHeight;
+// let ancho = window.screen.availWidth;
 
-comprar = confirm(`el alto es${alto}, el ancho es ${ancho}`);
+// comprar = confirm(`el alto es${alto}, el ancho es ${ancho}`);
 
-if (comprar){
-  alert("compra realizada exitosamente")
-} else{
-  alert("compra cancelada")
-}
- // PARTE 2: Cofla llega a su casa, abre el navegador y aprieta F11, desde ahi se pone a navegar pero hay un problema... como ahora está en
- // pantalla completa, no puede ver ni la barra de marcadores, ni el protocolo, ni la url, ni ninguna información que nos puede
- // otorgar la interfaz que contiene la barra de búsqueda...
+// if (comprar){
+//   alert("compra realizada exitosamente")
+// } else{
+//   alert("compra cancelada")
+// }
 
- //         - Crear un sismeta que muestre los suficientes datos como para conocer el sitio web
+// PARTE 2: Cofla llega a su casa, abre el navegador y aprieta F11, desde ahi se pone a navegar pero hay un problema... como ahora está en
+// pantalla completa, no puede ver ni la barra de marcadores, ni el protocolo, ni la url, ni ninguna información que nos puede
+// otorgar la interfaz que contiene la barra de búsqueda...
+
+//         - Crear un sismeta que muestre los suficientes datos como para conocer el sitio web
+
+let href = window.location.href;
+let pathname = window.location.pathname;
+let hostname = window.location.hostname;
+let protocol = window.location.protocol;
+
+let html = `Protocolo: <b>${protocol}</b><br>`;
+html += `Hostname: <b>${hostname}</b><br>`;
+html += `Pathname: <b>${pathname}</b><br>`;
+html += `URL Completa: <b>${href}</b><br>`;
+
+const titulo = document.querySelector(".titulo");
+titulo.innerHTML = html;
